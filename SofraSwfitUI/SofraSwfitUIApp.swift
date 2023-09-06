f8dfce0e71a80d1c608987357d2b582f3b20ff0d
erenolkun <erenolkun@gmail.com>
2023-09-06 08:25:00 +0300

    Initial Commit

---
+++ SofraSwfitUI/SofraSwfitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SofraSwfitUIApp.swift
+//  SofraSwfitUI
+//
+//  Created by FERDA OLKUN on 6.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SofraSwfitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
